2b0805bb6074f3769c9d3fa2d7e72386a880fd6c
3EGaming <3EGaming@users.noreply.github.com>
2020-08-27 14:56:43 +0100

    Update preview system to use fetch

--- files/js/readfile.js
+++ files/js/readfile.js
@@ -10,6 +10,27 @@ function readFile(filename) {
   return result;
 }
 
+function readFileNew(filename) {
+  fetch(filename)
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.text().then(function(data) {
+        return data;
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+}
+
 function makeStringIntoList(instring) {
   return (instring.split("\n"));
 }
