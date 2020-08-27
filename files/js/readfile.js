cfb867b598fdd65bc977df74904a5292bf5ba8a3
3EGaming <3EGaming@users.noreply.github.com>
2020-08-27 14:36:38 +0100

    Add system for displaying previews on project page

---
+++ files/js/readfile.js
@@ -0,0 +1,22 @@
+function readFile(filename) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filename, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+function makeStringIntoList(instring) {
+  return (instring.split("\n"));
+}
+
+function displayPreviews(folderlist) {
+  var result = "";
+  for(var i in folderlist) {
+    result.concat('<A href="', i, '"><iframe src="', i, '/preview/" height="90" width="50%" scrolling="no"></iframe></A><P></P>')
+  }
+  return result;
+}
