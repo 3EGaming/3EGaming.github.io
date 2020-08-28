001b5caccd4206d8e20ee923bd7f58ac9f94d706
3EGaming <3EGaming@users.noreply.github.com>
2020-08-28 14:37:52 +0100

    Update preview system

--- files/js/readfile.js
+++ files/js/readfile.js
@@ -29,7 +29,7 @@ function displayPreviews(folderlist) {
   window.resulte = "";
   for(i in folderlist) {
     console.log("Adding item: ".concat(folderlist[i]))
-    window.resulte = window.resulte.concat('<div style="position:relative;"><iframe src="', folderlist[i], '/preview/" height="105" width="50%" scrolling="no"></iframe><A href="', folderlist[i], '" style="position:absolute; top:0; left:0; display:inline-block; width:50%; height:105px; z-index:5;"></A></div><P></P>')
+    window.resulte = window.resulte.concat('<div style="position:relative;"><iframe src="', folderlist[i], '/preview/" height="105" width="35%" scrolling="no"></iframe><A href="', folderlist[i], '" style="position:absolute; top:0; left:0; display:inline-block; width:50%; height:105px; z-index:5;"></A></div><P></P>')
   }
   return window.resulte;
 }
