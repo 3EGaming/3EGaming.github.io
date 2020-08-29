bce22c1cc6f5d80216641678015da7264ac17717
3EGaming <3EGaming@users.noreply.github.com>
2020-08-29 19:28:10 +0100

    Create fullscreen.js

---
+++ files/js/fullscreen.js
@@ -0,0 +1,16 @@
+function goFullscreen() {
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+
+// Launch fullscreen for browsers that support it!
+launchFullScreen(document.documentElement); // the whole page
+launchFullScreen(document.getElementById("videoElement")); // any individual element
+}
