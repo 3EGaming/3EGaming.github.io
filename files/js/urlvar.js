43998e86ab9f2b0b1bf4528744e07d6d7d3c3c59
3EGaming <3EGaming@users.noreply.github.com>
2020-09-01 16:51:09 +0100

    More work on UNDERTALE Dialogue Generator

---
+++ files/js/urlvar.js
@@ -0,0 +1,13 @@
+var urlVariables = (function(var_a) {
+    if (var_a == "") return {};
+    var var_b = {};
+    for (var i = 0; i < var_a.length; ++i)
+    {
+        var var_p=var_a[i].split('=', 2);
+        if (var_p.length == 1)
+            var_b[var_p[0]] = "";
+        else
+            var_b[var_p[0]] = decodeURIComponent(var_p[1].replace(/\+/g, " "));
+    }
+    return var_b;
+})(window.location.search.substr(1).split('&'));
