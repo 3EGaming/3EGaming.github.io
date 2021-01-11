69ace51e62c458468fedb1b9631a0eed8b8bf943
3EGaming <3EGaming@users.noreply.github.com>
2021-01-11 10:58:37 +0000

    Update timedate_autorefresh.js

--- files/js/timedate_autorefresh.js
+++ files/js/timedate_autorefresh.js
@@ -1,13 +1,13 @@
 var refreshtimedate = function()
 {
 var timestuffs = new Date();
-var window.SECOND = timestuffs.getSeconds()
-var window.MINUTE = timestuffs.getMinutes()
-var window.HOUR = timestuffs.getHours()
-var window.DAY = timestuffs.getDay()
-var window.DATE = timestuffs.getDate()
-var window.MONTH = timestuffs.getMonth() + 1 //add 1 because the getMonth() function starts from 0 (January is 0 normaly)
-var window.YEAR = timestuffs.getFullYear()
+window.SECOND = timestuffs.getSeconds()
+window.MINUTE = timestuffs.getMinutes()
+window.HOUR = timestuffs.getHours()
+window.DAY = timestuffs.getDay()
+window.DATE = timestuffs.getDate()
+window.MONTH = timestuffs.getMonth() + 1 //add 1 because the getMonth() function starts from 0 (January is 0 normaly)
+window.YEAR = timestuffs.getFullYear()
 setTimeout(refreshtimedate, 1000);
 }
 
