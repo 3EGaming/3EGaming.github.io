26c7380cf64ce25f73dc06f854d0e5f2bac33450
3EGaming <3EGaming@users.noreply.github.com>
2021-06-02 22:21:18 +0100

    Fix some problems with paths

--- files/js/newtopbarload.js
+++ files/js/newtopbarload.js
@@ -1,4 +1,4 @@
-var topbarcontent = '<ul><li><img src="./files/image/Branding/LOGO3E.png" height="51" width="170" style="display: block;margin-right: 0.3px;"></li>';
+var topbarcontent = '<ul><li><img src="/files/image/Branding/LOGO3E.png" height="51" width="170" style="display: block;margin-right: 0.3px;"></li>';
 
 if (typeof thisPage == 'undefined') {
     var thisPage = "";
@@ -16,6 +16,6 @@ topbarcontent += ' href="/games/">Games</a></li><li><a';
 if (thisPage == "social") {
     topbarcontent += ' class="active"';
 }
-topbarcontent += ' href="/social/">Social</a></li><li style="float: right;"><a href="/settings/"><img src="./files/image/UI/settingsButton.png" height="25" width="25" style="display: block;"></a></li></ul>';
+topbarcontent += ' href="/social/">Social</a></li><li style="float: right;"><a href="/settings/"><img src="/files/image/UI/settingsButton.png" height="25" width="25" style="display: block;"></a></li></ul>';
 
 document.getElementById("topbar").innerHTML = topbarcontent;
