4fc7781863ea2e1dfddf89034ff517aa252938e4
3EGaming <3EGaming@users.noreply.github.com>
2021-06-03 12:26:55 +0100

    Fix a spelling mistake

--- files/js/maintenanceStatus.js
+++ files/js/maintenanceStatus.js
@@ -1,5 +1,5 @@
 var underMaintenance = true;
 
 if (underMaintenance) {
-    document.getElementById("extra").innerHTML = "<H3>The site is currantly under maintenance, so the page you are looking for may not be ready.</H3>";
+    document.getElementById("extra").innerHTML = "<H3>The site is currently under maintenance, so the page you are looking for may not be ready.</H3>";
 }
