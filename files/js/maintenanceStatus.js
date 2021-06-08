9cd636aef66beb49d3541b772f9820029416e1fa
3EGaming <3EGaming@users.noreply.github.com>
2021-06-08 22:12:14 +0100

    Update maintenanceStatus.js
    Pages that where missing have been created, so none of the links on the site should lead to 404

--- files/js/maintenanceStatus.js
+++ files/js/maintenanceStatus.js
@@ -1,4 +1,4 @@
-var underMaintenance = true;
+var underMaintenance = false;
 
 if (underMaintenance) {
     document.getElementById("extra").innerHTML = "<H3>The site is currently under maintenance, so the page you are looking for may not be ready.</H3>";
