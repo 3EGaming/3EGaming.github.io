077b4476f16f2030482696b896c65cdb80256079
3EGaming <coinman_1@icloud.com>
2021-09-21 09:26:23 +0100

    Update loadcss.js
    shorten file and free variable name for somthing else

--- files/js/loadcss.js
+++ files/js/loadcss.js
@@ -1,10 +1,8 @@
-var head = document.getElementsByTagName('HEAD')[0];
-
 function LoadCss(cssPath) {
     var thisCss = document.createElement('link');
     thisCss.rel = 'stylesheet';
     thisCss.type = 'text/css';
     thisCss.href = cssPath; 
 
-    head.appendChild(thisCss); 
+    document.getElementsByTagName('HEAD')[0].appendChild(thisCss); 
 }
