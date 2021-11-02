5e42a92fd246dd0aceddd9085f9dc76405188f57
3EGaming <coinman_1@icloud.com>
2021-11-02 09:56:28 +0000

    Update topbar load script with option to not load topbar for use in iframes

--- files/js/newtopbarload.js
+++ files/js/newtopbarload.js
@@ -1,17 +1,30 @@
-if (typeof thisPage == 'undefined') {
-    var thisPage = "";
-}
+var loadTopbar;
 
-function CheckPage(pageName) {
-    if (thisPage == pageName) {
-        return ' class="active"';
+if (urlVariables == undefined) {
+    loadTopbar = true;
+} else {
+    if (urlVariables["notopbar"] == "") {
+        loadTopbar = false;
     } else {
-        return '';
+        loadTopbar = true;
     }
 }
+if (loadTopbar) {
+    if (typeof thisPage == 'undefined') {
+        var thisPage = "";
+    }
 
-document.getElementById("topbar").innerHTML = '<ul><li><img src="/files/image/Branding/LOGO3E.png" height="51" width="170" style="display: block;margin-right: 0.3px;"></li><li><a' + 
-        CheckPage("home") + ' href="/">Home</a></li><li><a' + 
-        CheckPage("games") + ' href="/games/">Games</a></li><li><a' + 
-        CheckPage("social") + ' href="/social/">Social</a></li><li style="float: right;"' + 
-        CheckPage("settings") + '><a href="/settings/"><img src="/files/image/UI/settingsButton.png" height="24.5" width="24.5" style="display: block;"></a></li></ul><br><br>';
+    function CheckPage(pageName) {
+        if (thisPage == pageName) {
+            return ' class="active"';
+        } else {
+            return '';
+        }
+    }
+
+    document.getElementById("topbar").innerHTML = '<ul><li><img src="/files/image/Branding/LOGO3E.png" height="51" width="170" style="display: block;margin-right: 0.3px;"></li><li><a' + 
+            CheckPage("home") + ' href="/">Home</a></li><li><a' + 
+            CheckPage("games") + ' href="/games/">Games</a></li><li><a' + 
+            CheckPage("social") + ' href="/social/">Social</a></li><li style="float: right;"' + 
+            CheckPage("settings") + '><a href="/settings/"><img src="/files/image/UI/settingsButton.png" height="24.5" width="24.5" style="display: block;"></a></li></ul><br><br>';
+}
