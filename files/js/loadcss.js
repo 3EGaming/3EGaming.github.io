b6a0310a844fa514890c3d573747dcc842b0d5fa
3EGaming <3EGaming@users.noreply.github.com>
2022-02-28 22:36:39 +0000

    more cleaning, relocate some files, fix some metadata tags

--- files/js/loadcss.js
+++
@@ -1,8 +0,0 @@
-function LoadCss(cssPath) {
-    var thisCss = document.createElement('link');
-    thisCss.rel = 'stylesheet';
-    thisCss.type = 'text/css';
-    thisCss.href = cssPath; 
-
-    document.getElementsByTagName('HEAD')[0].appendChild(thisCss); 
-}
