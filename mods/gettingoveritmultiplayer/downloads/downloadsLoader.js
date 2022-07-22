a7fa33e2bf0aa6db03351d0ed4559dabd4b86415
3EGaming <coinman_1@icloud.com>
2022-07-22 20:49:05 +0100

    change link colour

--- mods/gettingoveritmultiplayer/downloads/downloadsLoader.js
+++ mods/gettingoveritmultiplayer/downloads/downloadsLoader.js
@@ -4,6 +4,6 @@ var sortedVersions = versions.sort((a, b) => b-a);
 
 var isFirst = true;
 versions.forEach(version => {
-    downloadsArea.innerHTML += `<H${isFirst?1:2}>V${version} - <a href="./files/GettingOverItMultiplayer_V${version}.zip">Download</a></HH${isFirst?1:2}>`;
+    downloadsArea.innerHTML += `<H${isFirst?1:2} class="info">V${version} - <a href="./files/GettingOverItMultiplayer_V${version}.zip">Download</a></HH${isFirst?1:2}>`;
     isFirst = false;
 });
