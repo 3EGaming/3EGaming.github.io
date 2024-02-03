8425640e01772438e1ec611a5f192f1e4ca58748
3EGaming <coinman_1@icloud.com>
2024-02-03 16:43:10 +0000

    clean up some stuff

--- assets/scripts/site/maintenanceStatus.js
+++ assets/scripts/site/maintenanceStatus.js
@@ -1,5 +1,6 @@
 var underMaintenance = false;
 
-if (underMaintenance) {
+if (underMaintenance) 
+{
     document.getElementById("extra").innerHTML = "<H3>The site is currently under maintenance, so the page you are looking for may not be ready.</H3>";
 }
--- assets/scripts/site/navbar.js
+++ assets/scripts/site/navbar.js
@@ -1,27 +1,35 @@
-var tabs = [
+var tabs = 
+[
     ["home", "Home", "/"],
     ["games", "Games", "/games/"],
     ["social", "Social", "/social/"]
 ]; 
 
-function CheckActive(tabID) {
+function CheckActive(tabID) 
+{
     var classData = "";
-    if (typeof thisPage != "undefined") {
-        if (thisPage == tabID) {
+    if (typeof thisPage != "undefined") 
+    {
+        if (thisPage == tabID) 
+        {
             classData = "class=\"active\"";
         }
     }
     return classData;
 }
 
-function LoadNavBar() {
-    if (typeof urlVariables != "undefined") {
-        if (urlVariables["notopbar"] == "") {
+function LoadNavBar() 
+{
+    if (typeof urlVariables != "undefined") 
+    {
+        if (urlVariables["notopbar"] == "") 
+        {
             return;
         }
     }
-    var barContent = "<li><img src=\"/assets/images/branding/LOGO3E.png\" height=\"51\" width=\"170\" style=\"display: block;margin-right: 0.3px;\" alt=\"3EGaming\"></li>";
-    tabs.forEach(function (tab) {
+    var barContent = "<li><div class=\"navbarLogo\"></div></li>";
+    tabs.forEach(function (tab) 
+    {
         barContent += ("<li><a " + CheckActive(tab[0]) + " href=\"" + tab[2] + "\">" + tab[1] + "</a></li>");
     });
     barContent += ("<li style=\"float: right;\" " + CheckActive("settings") + "><a href=\"/settings/\"><img src=\"/assets/images/UI/settingsButton.png\" height=\"23\" width=\"23\" style=\"display: block;\"></a></li>");
